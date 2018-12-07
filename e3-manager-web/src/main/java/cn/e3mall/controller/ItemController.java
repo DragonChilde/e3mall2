0f0bf820d3293279e5e4870421ebee12d36a5abc
dragon <fantasydragon_echo@163.com>
2018-12-07 18:00:49 +0800

    2018-12-07

--- e3-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
+++ e3-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
@@ -2,14 +2,17 @@ package cn.e3mall.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.e3mall.common.pojo.EasyUIDataGridResult;
 import cn.e3mall.common.utils.E3Result;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemDesc;
 import cn.e3mall.service.ItemService;
 
 @Controller
@@ -27,7 +30,7 @@ public class ItemController {
 	
 	@RequestMapping("/item/list")
 	@ResponseBody
-	public EasyUIDataGridResult getItemList(int page,int rows)
+	public EasyUIDataGridResult getItemList(@RequestParam(defaultValue="1")int page,@RequestParam(defaultValue="30")int rows)
 	{
 		EasyUIDataGridResult result = itemService.getItemList(page, rows);
 		return result;
@@ -39,6 +42,15 @@ public class ItemController {
 	{
 		E3Result result =  itemService.addItem(item, desc);
 		
+		return result;
+	}
+	
+	@RequestMapping("/item/desc/{itemId}")
+	@ResponseBody
+	public E3Result getItemDesc(@PathVariable Long itemId)
+	{
+		TbItemDesc itemDesc = itemService.getItemDescById(itemId);
+		E3Result result = E3Result.ok(itemDesc);
 		return result;
 	}
 }
